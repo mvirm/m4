5cb949c268b93ed40030e7a2de8fce9b06c01044
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-03-27 23:04:58 -0300

    first commit

---
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -0,0 +1,22 @@
+/* ¡Escribe tus comandos en este archivo! */
+
+const ejercicio02 = "SELECT * FROM movies WHERE duration < 90";
+ 
+const ejercicio03 = 'SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940';
+
+const ejercicio04 = "SELECT * FROM movies WHERE title LIKE '%til%'";
+
+const ejercicio05 = 'SELECT * FROM movies WHERE CARDINALITY(actors) = 1';
+
+const ejercicio06 = 'SELECT title, AVG(rating) FROM movies, UNNEST(ratings) AS rating GROUP BY title';
+
+const ejercicio07 = "SELECT actors FROM movies WHERE title LIKE '%Fast and%' AND year=2016";
+
+module.exports = {
+   ejercicio02,
+   ejercicio03,
+   ejercicio04,
+   ejercicio05,
+   ejercicio06,
+   ejercicio07,
+};
